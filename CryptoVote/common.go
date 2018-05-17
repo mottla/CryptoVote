9e0911fe6a93e4e485b80c51f26f47d1ce976637
mottla <a>
2018-05-17 10:30:42 +0200

    paper 0.1 upload  and licence modifications and insertations

--- CryptoVote/common.go
+++ CryptoVote/common.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2018-2019 by mottla
+// Copyright (c) 2015 The btcsuite developers
+// Copyright (c) 2015-2016 The Decred developers
+// Use of this source code is governed by an MIT
+// license that can be found in the LICENSE file.
 package main
 
 import (
--- CryptoVote/transaction.go
+++ CryptoVote/transaction.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2018-2019 by mottla
+// Use of this source code is governed by an MIT
+// license that can be found in the LICENSE file.
 package main
 
 import (
